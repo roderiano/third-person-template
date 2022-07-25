fe349b6a90ada84bf60f28dd3398f88b1fa2e7a3
unknown <gabrielsilveirapro@gmail.com>
2022-07-24 23:34:02 -0300

    General improvements

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Transform cameraTransform;
-    public float playerSpeed, currentSpeed = 3.0f;
+    public float playerSpeed = 3.0f;
+    public float jumpForce = 1f;
     
     private CharacterController characterController;
     private Vector3 velocity;
@@ -13,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
     private Transform modelTransform;
     private string currentAnimationName;
     private Animator animController;
+    private float currentSpeed;
+    private bool isAiming, isRunning;
 
     private void Start()
     {
@@ -20,7 +23,6 @@ public class PlayerMovement : MonoBehaviour
         characterController = transform.GetComponent<CharacterController>();
         modelTransform = transform.Find("PlayerModel");
         animController = modelTransform.GetComponent<Animator>();
-
         currentSpeed = playerSpeed;
     }
 
@@ -35,52 +37,62 @@ public class PlayerMovement : MonoBehaviour
 
     void Move() 
     {
+        // Get state values
         Vector3 directionInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        bool isAiming = Input.GetKey(KeyCode.Mouse1) && characterController.isGrounded;
-        bool isRunning = Input.GetKey(KeyCode.LeftShift) && !isAiming && characterController.isGrounded && directionInput != Vector3.zero;
+        isAiming = Input.GetKey(KeyCode.Mouse1) && characterController.isGrounded;
+        isRunning = Input.GetKey(KeyCode.LeftShift) && !isAiming && characterController.isGrounded && directionInput != Vector3.zero;
         
         // Set running state
-        currentSpeed = isRunning ? playerSpeed + (playerSpeed * 0.5f) : playerSpeed;
         animController.SetBool("Running", isRunning);
-
-        // Get directions
-        Vector3 directionToMove = isAiming ? modelTransform.TransformDirection(directionInput).normalized : cameraTransform.TransformDirection(directionInput).normalized;
-        directionToMove.y = -0.1f;
 
         // Move
         if(directionInput != Vector3.zero)
         {
+            // Get directions
+            Vector3 directionToMove = isAiming ? modelTransform.TransformDirection(directionInput).normalized : cameraTransform.TransformDirection(directionInput).normalized;
+            directionToMove.y = -0.1f;
+            
+            currentSpeed = isRunning ? playerSpeed + (playerSpeed * 0.5f) : playerSpeed;
             characterController.Move(directionToMove * Time.deltaTime * currentSpeed);
         }
-
-        animController.SetFloat("MoveDirectionX", directionInput.x, 1f, Time.deltaTime * 10f);
-        animController.SetFloat("MoveDirectionY", directionInput.z, 1f, Time.deltaTime * 10f);
-
+        
         // Rotate
-        Vector3 directionToRotate = isAiming ? cameraTransform.forward : cameraTransform.TransformDirection(directionInput).normalized;
-        directionToRotate.y = 0f;
-        if(directionToMove != Vector3.zero)
-            modelTransform.rotation = Quaternion.Slerp(modelTransform.rotation, Quaternion.LookRotation(directionToRotate), 10f * Time.deltaTime); 
+        if(isAiming || directionInput != Vector3.zero)
+        {  
+            Vector3 directionToRotate = isAiming ? cameraTransform.forward : cameraTransform.TransformDirection(directionInput).normalized;
+            directionToRotate.y = 0f;
+            modelTransform.rotation = Quaternion.Slerp(modelTransform.rotation, Quaternion.LookRotation(directionToRotate), 10f * Time.deltaTime);
+        } 
     
         // Set Animator Params
         animController.SetBool("Jog", directionInput != Vector3.zero);
         animController.SetBool("Aiming", isAiming);
+        animController.SetFloat("MoveDirectionX", directionInput.x, 1f, Time.deltaTime * 10f);
+        animController.SetFloat("MoveDirectionY", directionInput.z, 1f, Time.deltaTime * 10f);
     }
 
     void ApplyGravity() 
     {
+        // Force ground collision
         if (characterController.isGrounded && velocity.y < 0)
         {
+            velocity = Vector3.zero;
             velocity.y = -1f;
         }
 
-        Debug.Log(characterController.isGrounded);
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
         {
             animController.SetTrigger("Jump");
-            velocity.y += Mathf.Sqrt(1.5f * -3.0f * gravityValue);
+            
+            // Increment jump force when running
+            if(isRunning)
+                velocity = modelTransform.forward.normalized;
+
+            velocity.y += Mathf.Sqrt(jumpForce * -3.0f * gravityValue);
         }
 
+        // Apply gravity
         velocity.y += gravityValue * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
 
